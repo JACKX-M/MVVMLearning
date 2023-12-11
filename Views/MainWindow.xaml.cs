851eac0b7428b8dc28f3c9c170af8821478b79f7
JACKX-M <2817980855@qq.com>
2023-12-12 01:24:48 +0800

    customized title bar

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVMLearning.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btn_maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                btn_maximize_image.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/Images/maximize.png"));
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                btn_maximize_image.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/Images/maximized.png"));
+            }
+        }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
